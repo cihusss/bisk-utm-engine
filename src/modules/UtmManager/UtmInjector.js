de353e99cc06f8d11afaadd4f664cb2cfc309e71
cihusss <cihusss@gmail.com>
2024-01-18 23:32:32 +0200

    some more cleanup

--- src/modules/UtmManager/UtmInjector.js
+++ src/modules/UtmManager/UtmInjector.js
@@ -5,6 +5,11 @@ export const UtmInjector = (utmData) => {
       // form.addHiddenFields({"Results_PDF_URL__c":"assignedCampaignID"});
       for (let key in utmData) {
         if (utmData[key].utm || utmData[key].localStorage) {
+          console.log(
+            'Injecting hidden fields to form:',
+            utmData[key].label,
+            utmData[key].utm || utmData[key].localStorage
+          );
           form.addHiddenFields({
             [utmData[key].label]: utmData[key].utm || utmData[key].localStorage,
           });
