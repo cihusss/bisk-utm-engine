7f2808ebe09d353ff3e1e125ca54d87f9dcff33c
cihusss <cihusss@gmail.com>
2024-01-18 22:06:59 +0200

    bug fix...

--- src/modules/UtmManager/UtmInjector.js
+++ src/modules/UtmManager/UtmInjector.js
@@ -1,8 +1,10 @@
 export const UtmInjector = (kkk, ppp) => {
+  console.log('running UTM injector...');
   // Inject hidden fields into Marketo form with localStorage values
   if (typeof MktoForms2 != 'undefined') {
     MktoForms2.whenReady(function (form) {
       // form.addHiddenFields({"Results_PDF_URL__c":"assignedCampaignID"});
+      console.log('injecting hidden fields...');
       form.addHiddenFields({
         kkk: ppp,
       });
