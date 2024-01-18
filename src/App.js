697387255118ebe7dfb8f83f7063efca9bae96ca
cihusss <cihusss@gmail.com>
2024-01-18 23:47:32 +0200

    a bit of rework again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { UtmManager } from './modules/UtmManager/UtmManager';
 import { UtmInjector } from './modules/UtmManager/UtmInjector';
 const App = () => {
-  const utmData = {};
-
   window.addEventListener('load', (e) => {
-    UtmManager(utmData);
+    const utmData = UtmManager();
     UtmInjector(utmData);
   });
 };
